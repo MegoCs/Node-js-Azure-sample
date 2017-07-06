4f4ee03b9c876cf4280f6494f070d8a4fb2a51d7
Ahmed Magdy <MegoCs@hotmail.com>
2017-07-06 02:32:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-const express = require('express')
-const app = express()
-
 app.get('/', function (req, res) {
   res.send('Hello Azure, This is Node.js!')
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
 })
